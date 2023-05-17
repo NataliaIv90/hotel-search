fd29cd5c621175047706820c22bcab0e0422bdba
Natalia Ivantsova <ivantsovana@gmail.com>
2023-05-17 13:19:20 +0300

    feat: add insertion by js cards with photos for gallery and icons for flight details

---
+++ script/detail.js
@@ -0,0 +1,24 @@
+function displayGalleryImg() {
+  const galleryImgArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    galleryDiv = document.querySelector(".planeInfo__galleryCards");
+  galleryImgArr.map(el => {
+    galleryDiv.insertAdjacentHTML("beforeend", `
+    <div class="planeInfo__galleryCardsImgContainer" style="background-image:url('../../../img/flight-details/plane-info/${el}.jpg')"></div>
+    `);
+  })
+}
+
+displayGalleryImg();
+
+function displayCardInfoIcons() {
+  const infoIconsArr = ["plane-dark", "wifi", "watch", "food", "seat"],
+    infoIconsDiv = document.querySelector(".flightInfo__cardInfoIconsWrapper");
+
+  infoIconsArr.map(el => {
+    infoIconsDiv.insertAdjacentHTML("beforeend", `
+    <div><img src="../../../img/icons/${el}.svg" alt=""></div>
+    `)
+  })
+}
+
+displayCardInfoIcons();
